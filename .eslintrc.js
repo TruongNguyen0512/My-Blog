85406b19698dbe398d99540632eb1907e55595be
TruongNguyen0512 <nguyencbsw.thcs@gmail.com>
2024-04-13 01:41:38 +0700

    blog-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Welcome to My Blog!</h1>
+      <p>This is the homepage of our blog application.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
+  <style scoped>
+  /* CSS styles for the homepage component */
+  </style>
+  
