43dae0654088d5502948db2962001b8d397b3988
TruongNguyen0512 <nguyencbsw.thcs@gmail.com>
2024-04-14 11:18:15 +0700

    update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,83 +1,195 @@
 <template>
     <header>
-      <div class="logo">My Blog</div>
-      <nav>
-        <ul>
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/about">About</router-link></li>
-          <!-- Thêm các liên kết khác của menu điều hướng -->
-        </ul>
-      </nav>
-      <div class="user">
-        <button @click="toggleUserMenu">User</button>
-        <div v-if="showUserMenu" class="user-menu">
-          <router-link to="/login">Login</router-link>
-          <router-link to="/register">Register</router-link>
+    <div class="logo">My Blog</div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact-us">Contact us</router-link></li>
+        <!-- Thêm các liên kết khác của menu điều hướng -->
+      </ul>
+    </nav>
+    <div class="user">
+      <button @click="toggleModal">User</button>
+      <!-- Sử dụng modal -->
+      <div v-if="showModal" class="modal">
+        <div class="modal-content">
+          <!-- Form đăng nhập -->
+          <form v-if="isLoginFormVisible" @submit.prevent="login">
+            <h2>Login</h2>
+            <div>
+              <label for="login-email">Email:</label>
+              <input type="text" id="login-email" v-model="email" required>
+            </div>
+            <div>
+              <label for="login-password">Password:</label>
+              <input type="password" id="login-password" v-model="password" required>
+            </div>
+            <button type="submit">Login</button>
+            <p>Don't have an account? <a  href="#" @click="toggleForm">Register now</a></p>
+          </form>
+          <!-- Form đăng ký -->
+          <form v-if="isRegisterFormVisible" @submit.prevent="register">
+            <h2>Register</h2>
+            <div>
+              <label for="register-username">Username:</label>
+              <input type="text" id="register-username" v-model="username" required>
+            </div>
+            <div>
+              <label for="register-email">Email:</label>
+              <input type="text" id="register-email" v-model="email" required>
+            </div>
+            <div>
+              <label for="register-password">Password:</label>
+              <input type="password" id="register-password" v-model="password" required>
+            </div>
+            <button type="submit">Register</button>
+            <p>Already have an account? <a href="#" @click="toggleForm">Login</a></p>
+          </form>
         </div>
       </div>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        showUserMenu: false
-      };
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+      isLoginFormVisible: true,
+      isRegisterFormVisible: false,
+      username: '',
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+      // Mặc định hiển thị form đăng nhập khi mở modal
+      this.isLoginFormVisible = true;
+      // Ẩn form đăng ký khi mở modal
+      this.isRegisterFormVisible = false;
     },
-    methods: {
-      toggleUserMenu() {
-        this.showUserMenu = !this.showUserMenu;
-      }
+    toggleForm() {
+      // Chuyển đổi giữa form đăng nhập và form đăng ký
+      this.isLoginFormVisible = !this.isLoginFormVisible;
+      this.isRegisterFormVisible = !this.isRegisterFormVisible;
+    },
+    login() {
+      // Xử lý đăng nhập ở đây
+      // Sau khi đăng nhập thành công, có thể chuyển hướng người dùng đến trang khác
+    },
+    register() {
+      // Xử lý đăng ký ở đây
+      // Sau khi đăng ký thành công, có thể chuyển hướng người dùng đến trang khác
     }
-  };
-  </script>
-  
-  <style scoped>
-  header {
+  }
+};
+</script>
+
+<style scoped>
+header {
+  height: 50px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 1rem;
     background-color: #333;
     color: #fff;
-  }
-  
-  .logo {
-    font-size: 1.5rem;
-  }
-  
-  nav ul {
-    list-style: none;
-    display: flex;
-  }
-  
-  nav ul li {
-    margin-right: 1rem;
-  }
-  
-  .user-menu {
+}
+
+
+
+.logo {
+  font-size: 1.5rem;
+}
+
+nav{
+      /* margin: 0px; */
+      position: absolute;
+    left: 100px;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+}
+
+nav ul li {
+  margin-right: 1rem;
+}
+
+.user-menu {
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 0.5rem;
+  display: none;
+}
+
+.modal {
+  position: fixed;
+  top: 90px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Mờ đen */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content[data-v-bb50a5e4] {
+  height: 305px;
+    width: 510px;
+    background-color: #543c3c;
+    padding: 1rem;
+    border-radius: 8px;
     position: absolute;
-    top: 100%;
-    right: 0;
-    background-color: #fff;
+    top: 30px;
+}
+
+.modal form {
+  margin-bottom: 1rem;
+}
+
+.modal form div {
+  margin-bottom: 0.5rem;
+}
+
+.modal form label {
+  display: block;
+  margin-bottom: 0.25rem;
+}
+
+.modal form input {
+  width: 100%;
+  
+    height: 29px;
     border: 1px solid #ccc;
-    padding: 0.5rem;
-    display: none;
-  }
-  
-  .user-menu a {
+    border-radius: 4px;
+}
+
+h2 {
     display: block;
-    color: #333;
-    text-decoration: none;
-  }
-  
-  .user-menu a:hover {
-    background-color: #f4f4f4;
-  }
-  
-  .user:hover .user-menu {
-    display: block;
-  }
-  </style>
-  
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    unicode-bidi: isolate;
+}
+a{
+  text-decoration: none ;
+  color: #fff; /* Màu chữ sáng */
+}
+a:hover {
+  color: #ccc; /* Màu chữ thay đổi khi di chuột qua */
+}
+
+</style>
